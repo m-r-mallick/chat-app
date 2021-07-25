1fc28f178520c824b879a462d8f773f840b96df6
codeworm <mrmblogger@gmail.com>
2021-07-25 20:07:53 +0530

    added responsive dashboard

--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -1,8 +1,26 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useState, useEffect } from 'react';
 
 export function useModelState(defaultValue = false) {
    const [isOpen, setIsOpen] = useState(defaultValue);
    const open = useCallback(() => setIsOpen(true), []);
    const close = useCallback(() => setIsOpen(true), []);
    return { isOpen, open, close };
+}
+
+export function useMediaQuery(query) {
+   const [matches, setMatches] = useState(
+      () => window.matchMedia(query).matches
+   );
+
+   useEffect(() => {
+      const queryList = window.matchMedia(query);
+      setMatches(queryList.matches);
+
+      const listener = evt => setMatches(evt.matches);
+
+      queryList.addListener(listener);
+      return () => queryList.removeListener(listener);
+   }, [query]);
+
+   return matches;
 }
