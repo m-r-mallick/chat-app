7645c70e1f977171b27896daa0cd89bd95ffe691
codeworm <mrmblogger@gmail.com>
2021-07-28 00:59:21 +0530

    started working with chatroom list

---
+++ src/components/ChatRoomList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { List, Nav } from 'rsuite';
+import RoomItem from './RoomItem';
+
+const ChatRoomList = ({ elHeight }) => {
+   return (
+      <Nav
+         appearance="subtle"
+         vertical
+         reversed
+         className="overflow y-scroll custom-scroll"
+         style={{ height: `calc(100% - ${elHeight}px)` }}
+      >
+         <Nav.Item>
+            <RoomItem />
+         </Nav.Item>
+      </Nav>
+   );
+};
+
+export default ChatRoomList;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { Divider } from 'rsuite';
+import ChatRoomList from './ChatRoomList';
 import CreateRoomBtnModal from './CreateRoomBtnModal';
 import DashboardToggle from './dashboard/DashboardToggle';
 
 const Sidebar = () => {
+   const topSideBarRef = useRef();
+   const [height, setHeight] = useState(0);
+
+   useEffect(() => {
+      if (topSideBarRef.current) {
+         setHeight(topSideBarRef.current.scrollHeight);
+      }
+   }, [topSideBarRef]);
+
    return (
       <div className="h-100 pt-2">
-         <div>
+         <div ref={topSideBarRef}>
             <DashboardToggle />
             <CreateRoomBtnModal />
+            <Divider>Join Conversation</Divider>
          </div>
-         bottom
+         <ChatRoomList elHeight={height} />
       </div>
    );
 };
