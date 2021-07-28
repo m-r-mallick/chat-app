1958149da6db770941c0bbf44b054a5ff3d8d718
codeworm <mrmblogger@gmail.com>
2021-07-29 02:50:36 +0530

    displayed user data modal window

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import TimeAgo from 'timeago-react';
 import ProfileAvatar from '../../ProfileAvatar';
+import ProfileInfoBtnModal from './ProfileInfoBtnModal';
 
 const MessageItem = ({ message }) => {
    const { author, createdAt, text } = message;
 
    return (
-      <div>
+      <>
          <li className="padded mb-1">
             <div className="d-flex align-items-center font-bolder mb-1">
                <ProfileAvatar
@@ -15,7 +16,11 @@ const MessageItem = ({ message }) => {
                   className="ml-1"
                   size="xs"
                />
-               <span className="ml-2">{author.name}</span>
+               <ProfileInfoBtnModal
+                  profile={author}
+                  appearance="link"
+                  className="p-0 ml-1 text-black"
+               />
                <TimeAgo
                   datetime={createdAt}
                   className="font-normal text-black-45 ml-2"
@@ -25,7 +30,7 @@ const MessageItem = ({ message }) => {
                <span className="word-breal-all">{text}</span>
             </div>
          </li>
-      </div>
+      </>
    );
 };
 
